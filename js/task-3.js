302e6ae408d243804600543c320f7f457a0c6ce2
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-06-18 20:06:12 +0300

    commit message

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function getElementWidth(content, padding, border) {
+  let contentWidth = Number.parseFloat(content);
+  let paddingWidth = Number.parseFloat(padding);
+  let bordertWidth = Number.parseFloat(border);
+
+  // console.log(contentWidth);
+  // console.log(paddingWidth);
+  // console.log(bordertWidth);
+
+  let totalWidth = contentWidth + paddingWidth * 2 + bordertWidth * 2;
+
+  return totalWidth;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
