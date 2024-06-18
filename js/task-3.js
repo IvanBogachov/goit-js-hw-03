0ac395a5895c3d6a5463577185d1bfc1f4768bb9
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-06-18 20:21:24 +0300

    commit message

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,14 @@
-function getElementWidth(content, padding, border) {
-  let contentWidth = Number.parseFloat(content);
-  let paddingWidth = Number.parseFloat(padding);
-  let bordertWidth = Number.parseFloat(border);
-
-  // console.log(contentWidth);
-  // console.log(paddingWidth);
-  // console.log(bordertWidth);
-
-  let totalWidth = contentWidth + paddingWidth * 2 + bordertWidth * 2;
-
-  return totalWidth;
+function filterArray(numbers, value) {
+  const newNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      newNumbers.push(numbers[i]);
+    }
+  }
+  return newNumbers;
 }
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
